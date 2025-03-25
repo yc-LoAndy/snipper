572649fcf1e9135cb1055974d6940253332c4a06
yc-LoAndy <andyyuanchun@gmail.com>
2025-03-25 22:06:37 +0800

    chore: lint

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,14 +1,16 @@
 import { createHash } from "crypto"
 import prisma from "./prisma"
-import { folder } from "@prisma/client";
+import { folder } from "@prisma/client"
+import * as g from "@/globalVars"
+import { NotFoundError } from "@/models/errors"
 
 export function trimAny(str: string, chars: string[]) {
     let start = 0, end = str.length;
 
-    while(start < end && chars.indexOf(str[start]) >= 0)
+    while (start < end && chars.indexOf(str[start]) >= 0)
         ++start;
 
-    while(end > start && chars.indexOf(str[end - 1]) >= 0)
+    while (end > start && chars.indexOf(str[end - 1]) >= 0)
         --end;
 
     return (start > 0 || end < str.length) ? str.substring(start, end) : str;
