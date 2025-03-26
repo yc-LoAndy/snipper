06487a411a37733332ad952dd474155a2a507505
yc-LoAndy <andyyuanchun@gmail.com>
2025-03-26 20:36:43 +0800

    feat: mimicking internet delay

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,16 @@ import snippetRoute from "./snippets"
 import middlewares from "@/middlewares"
 
 const r = Router()
+// utility route to mimic internet delay
+/*
+import env from "@/utils/env"
+r.all('*', async (_, __, next) => {
+    const sleep = () => new Promise(resolve => setTimeout(resolve, 2*1000))
+    if (env.get("ENV") === "dev")
+        await sleep()
+    next()
+})
+*/
 r.use(authRoute)
 r.use(userRoute)
 r.use(snippetRoute)
