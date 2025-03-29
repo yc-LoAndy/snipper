f93a67f5a8d62505846b334583e221ca59b910ca
yc-LoAndy <andyyuanchun@gmail.com>
2025-03-29 17:28:25 +0800

    chore: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
     { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     {
         rules: {
-            'semi': ['error', 'never'],
-            'quotes': ['error', 'double'],
-            'no-undef': ['off']  // let typescript plugin handle it
+            "semi": ["error", "never"],
+            "quotes": ["error", "double"],
+            "no-undef": ["off"]  // let typescript plugin handle it
         }
     }
-];
+]
